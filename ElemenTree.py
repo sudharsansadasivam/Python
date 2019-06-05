11838800672d3e2aeb6859d867881d44734bac8d
Sudharsan Sadasivam <sudharsansadasivam@gmail.com>
2019-06-05 10:05:43 +0530

    XmlDataExtraction
    To Extract Specific Data from XMl using ELementTree Library in Python

---
+++ ElemenTree.py
@@ -0,0 +1,19 @@
+import xml.etree.ElementTree as ET
+import csv
+s_no=1
+f=open('xmlDataExtract.csv','w')
+my=csv.writer(f,delimiter=' ',lineterminator='\n')
+head=['s.no','Attribute Name','Visible Name']
+my.writerow(head)
+root = ET.parse('C:\\Users\\sudharsan.sadasivam\\Documents\\python\\attribute.xml').getroot()
+for type_tag in root.findall('typedef/attribute'):
+    print(type_tag)
+    attributeValue = type_tag.get('name')
+    visbileValue = type_tag.get('visible')
+    if visbileValue == None:
+        visbileValue = 'true'
+    print(attributeValue , visbileValue)
+    my.writerow([s_no,attributeValue,visbileValue])
+    s_no+=1
+    
+f.close()
