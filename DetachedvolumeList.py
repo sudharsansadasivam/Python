e101ec06cfe679bbf026941d3bfbe3e7bac73fcd
Sudharsan Sadasivam <sudharsansadasivam@gmail.com>
2022-04-29 12:08:27 +0530

    Delete DetachedvolumeList.py

--- DetachedvolumeList.py
+++
@@ -1,9 +0,0 @@
-#Script is to display all the detatched volumes in AWS 
-
-import boto3
-ec2 = boto3.resource('ec2', region_name='us-east-1')
-volumes = ec2.volumes.all() # If you want to list out all volumes
-volumes = ec2.volumes.filter(Filters=[{'Name': 'status', 'Values': ['available']}]) # if you want to list out only attached volumes
-
-for volume in volumes:
-    print (volume.id)
